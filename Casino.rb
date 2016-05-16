a35338c3f88865deca08fa68c925dc5642564537
Pat Marshall <pmarshall1716@gmail.com>
2016-05-16 12:14:32 -0600

    Casino Project

---
+++ Casino.rb
@@ -0,0 +1,16 @@
+#require_relative 'mencahnics/dice'
+Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file}
+class Casino
+  include Mechanics
+
+  def play
+    puts Mechanics::Dice.roll
+    cards = Mechanics::Deck.new.cards.shuffle
+    cards.each do |card|
+      puts "#{card.rank} of #{card.suit}"
+    end
+  end
+end
+
+@game = Casino.new
+@game.play
